e46052341239474587b8726d53c244034e01ac47
Bappi Saha <bappi35-2097@diu.edu.bd>
2024-08-29 14:27:04 +0600

    feat: adding until single digit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = function digitalRoot(number) {
+    while (number >= 10) {
+        number = number.toString()
+                       .split('')
+                       .reduce((sum, digit) => sum + Number(digit), 0);
+    }
+    return number;
+}
--- tests.test.js
+++ tests.test.js
@@ -1,3 +1,9 @@
+const digitalRoot = require('./index')
+
 test('dummy test', () => {
     expect(2).toBe(2);
-  });
+});
+
+test('it will add all digit until it on digit', () => {
+  expect(digitalRoot(38)).toBe(2)
+})
